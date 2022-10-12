99e5d39e238fa59c16e292731d9ebb23e25838a1
colin_bao <colin_bao@163.com>
2022-10-12 14:58:59 +0800

    update

--- check_airflow.py
+++ check_airflow.py
@@ -1,11 +1,12 @@
 import subprocess
+from time import time
 import yagmail
-
+import time
 import logging
 
 logging.basicConfig(level=logging.DEBUG,  # 控制台打印的日志级别
-                    filename='check_airflow.log',
-                    filemode='a',  # 模式，有w和a，w就是写模式，每次都会重新写日志，覆盖之前的日志, a是追加模式，默认如果不写的话，就是追加模式
+                    filename='log/check_airflow.log',
+                    filemode='a',  # 追加模式
                     # 日志格式
                     format='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s'
                     )
@@ -15,26 +16,37 @@ USER_PWD = "htfxbkerhthpbcfb"
 USER_HOST = "smtp.qq.com"
 CHECK_TIME = 1800
 SEND_TO = "523393445@qq.com"
+MAX_TRY = 2
+TRY_DELAY = 5
+# 发送邮件
 
 
 def send_mail(send_user, send_password, send_host, to_user, to_subject, to_contents):
     yag = yagmail.SMTP(user=send_user, password=send_password, host=send_host)
     yag.send(to_user, to_subject, to_contents)
 
+# 进程检测
+
 
 def check_process(Process):
     cmd = 'ps axu | grep %s | grep -v grep | wc -l' % Process
     res = subprocess.Popen(
         cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    logging.info(res)
 
+    # 获取返回值
     if res.stdout.read().decode('utf-8').strip() == '0':
         subject = '[AirFlow警告] 进程 %s 宕机' % Process
         msg = ['%s 已停止运行,请检查,日志文件详见schedulerout.log' % Process]
         logging.warning(msg)
+        # 重新启动
+        re_start_cmd='nohup airflow scheduler > log/schedulerout.log 2>&1 &'
+        subprocess.Popen(
+            re_start_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     else:
-        subject = '[AirFlow警告] 进程 % s 运行正常' % Process
-        msg = ['正常运行,日志文件详见schedulerout.log']
+        subject = '[AirFlow警告] 进程 %s 运行正常' % Process
+        msg = ['%s 正常运行,日志文件详见schedulerout.log'% Process]
         logging.info(msg)
 
     send_mail(USER_NAME, USER_PWD,
@@ -44,7 +56,16 @@ def check_process(Process):
 
 
 if __name__ == '__main__':
-    try:
-        check_process('scheduler')
-    except Exception as e:
-        logging.warning(e)
+    i = 0
+    while i < MAX_TRY:
+        try:
+            check_process('scheduler')
+            logging.info('check_process正常执行,已退出检测')
+            break
+        except Exception as e:
+            logging.warning(e)
+            i += 1
+            time.sleep(TRY_DELAY)
+            logging.info(f'check_process重新执行:{i}/{MAX_TRY},{TRY_DELAY}')
+            continue
+    
--- start.sh
+++ start.sh
@@ -1,8 +1,7 @@
 # 启动脚本start_airflow.sh
 #!/bin/bash
 # P@ssw0rd
-nohup airflow webserver --port 8081 > webserverout.log 2>&1 &
-nohup airflow scheduler > schedulerout.log 2>&1 &
-nohup python check_airflow.py > check_airflow.log 2>&1 &
+nohup airflow webserver --port 8081 > log/webserverout.log 2>&1 &
+nohup airflow scheduler > log/schedulerout.log 2>&1 &
 echo "Start success"
 
